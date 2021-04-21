1178ef4da8e6cfb63282f85cef918804d947b5e9
SineadManuel <c19336081@mytudublin.ie>
2021-04-21 16:24:19 +0100

    Constructor & toString Implemented

--- java/src/ie/tudublin/Note.java
+++ java/src/ie/tudublin/Note.java
@@ -3,4 +3,14 @@ package ie.tudublin;
 public class Note {
     private char note;
     private int duration;
+    
+    public Note(char note, int duration) {
+        this.note = note;
+        this.duration = duration;
+    }
+
+    @Override
+    public String toString() {
+        return "Note [note=" + note + ", duration=" + duration + "]";
+    }
 }
